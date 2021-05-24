d7f78980fd3e52dc0d6038ddc978e3c7fac54e6f
mauriciopc1 <mepinto1@uc.cl>
2021-05-24 14:47:53 -0400

    seguimiento vuelos en mapa

--- src/index.js
+++ src/index.js
@@ -2,10 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import io from "socket.io-client";
 import moment from "moment";
+import {
+  MapContainer,
+  Marker,
+  TileLayer,
+  Popup,
+  Polyline,
+} from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import "./map.css";
+import { IconLocation } from "./IconLocation";
+import { Icon, map } from "leaflet";
 
 const user = prompt("ingresar usuario");
-
-const element = <h1>Hello world</h1>;
 
 class Chat extends React.Component {
   constructor(props) {
@@ -59,13 +68,99 @@ class Chat extends React.Component {
   }
 }
 
+class MapRender extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      positions: {},
+      flights: [],
+    };
+  }
+
+  componentDidMount() {
+    this.socket = io("wss://tarea-3-websocket.2021-1.tallerdeintegracion.cl/", {
+      path: "/flights",
+      transports: ["websocket"],
+    });
+    this.socket.on("POSITION", (position) => {
+      const newJson = this.state.positions;
+      if (newJson[position.code] == undefined) {
+        newJson[position.code] = {};
+        newJson[position.code].position = position.position;
+      } else {
+        if (newJson[position.code].prevPos == undefined) {
+          newJson[position.code].prevPos = [];
+        } else {
+          newJson[position.code].prevPos = [
+            ...newJson[position.code].prevPos,
+            newJson[position.code].position,
+          ];
+        }
+        newJson[position.code].position = position.position;
+      }
+      this.setState({ positions: newJson });
+      console.log(this.state.positions);
+    });
+
+    this.socket.on("FLIGHTS", (flight) => {
+      this.setState({ flights: flight });
+      console.log(flight);
+    });
+  }
+
+  handleRequest = (event) => {
+    console.log("lo hicimos");
+    this.socket.emit("FLIGHTS");
+  };
+
+  render() {
+    let flights = [];
+    let paths = [];
+    for (const flight in this.state.positions) {
+      flights = [
+        <Marker position={this.state.positions[flight].position}>
+          <Popup>{flight}</Popup>
+        </Marker>,
+        ...flights,
+      ];
+      if (this.state.positions[flight].prevPos !== undefined) {
+        paths = [
+          <Polyline
+            pathOptions={{ color: "red" }}
+            positions={this.state.positions[flight].prevPos}
+          />,
+          ...paths,
+        ];
+      }
+    }
+    return (
+      <div>
+        <button onClick={this.handleRequest}>Pedir informacion</button>
+        <MapContainer center={{ lat: "4.6973985", lng: "13.41053" }} zoom={2}>
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          {flights}
+          {paths}
+        </MapContainer>
+      </div>
+    );
+  }
+}
+
 class App extends React.Component {
   constructor(props) {
     super(props);
   }
   render() {
-    return <div className="algo">{element}</div>;
+    return (
+      <div>
+        <MapRender />
+        <Chat />
+      </div>
+    );
   }
 }
 
-ReactDOM.render(<Chat />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
